fc93700754af248e20574caea8db6685cc10aa0e
satellaexidna <satellaexidna@gmail.com>
2026-05-13 09:56:03 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'storycraft-v1';
+const urlsToCache = [
+  '/storycraft-pwa/',
+  '/storycraft-pwa/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
